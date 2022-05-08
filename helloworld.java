5f3be6959ab54ac357037f2b4d02b4a6e20d91f4
jadhavaksh99 <jadhavaksh99@gmail.com>
2022-05-08 11:44:10 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World from GitHub using webhook checking changes");
+    System.out.println("Hello World from GitHub using webhook");
     }
 }
